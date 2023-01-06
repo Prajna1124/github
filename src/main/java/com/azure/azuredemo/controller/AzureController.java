1ca2636827ef88bfc6300a3b3feeed2cbd8dc044
Prajna <prajnaparimitak@gmail.com>
2023-01-06 13:56:31 +0530

    java file

--- src/main/java/com/azure/azuredemo/controller/AzureController.java
+++ src/main/java/com/azure/azuredemo/controller/AzureController.java
@@ -1,12 +1,14 @@
 package com.azure.azuredemo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AzureController {
 
-	@GetMapping("/message")
+	//@GetMapping("/message")
+	@RequestMapping(value = "/message")
 	public String getMessage() {
 		return "Welcome to azure demo !!!";
 	}
